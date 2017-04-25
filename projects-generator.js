34a848e1d502707cafd35af9e2c616f163dbdb82
Hideo Yasumoto <hideo54@hideo54.com>
2017-04-26 07:39:25 +0900

    Create projects generator (beta)

---
+++ projects-generator.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const data = require('./data.json');
+
+for (section of data.projects) {
+    for (project of section.contents) {
+const text = `extends ../_subpage.pug
+
+block head
+    title ${project.name}
+
+block main
+    h1 ${project.name}
+    p ${project.description}
+    h2 場所
+    p ${project.place}
+    //- ここにマップを入れる
+`;
+        fs.writeFile(`src/project/${project.address}.pug`, text);
+    }
+}
