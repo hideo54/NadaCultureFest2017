ed6a1df3a31dccb74b01eb68492cf8ecf445a843
Hideo Yasumoto <hideo54@hideo54.com>
2017-04-28 07:30:12 +0900

    各エリアのマップ表示の完成 (表記ゆれ訂正を含む))

--- projects-generator.js
+++ projects-generator.js
@@ -3,7 +3,7 @@ const data = require('./data.json');
 
 for (section of data.projects) {
     for (project of section.contents) {
-const text = `extends ../_subpage.pug
+let text = `extends ../_subpage.pug
 
 block head
     title ${project.name}
@@ -13,8 +13,32 @@ block main
     p ${project.description}
     h2 場所
     p ${project.place}
-    //- ここにマップを入れる
-`;
+    `;
+        if (project.place.indexOf('研修館') !== -1) {
+            text += 'include ../../images/2f.svg';
+            text += '\n    '
+            text += `style g#west > rect { fill: #faa95b !important; }`;
+        } else if (project.place.indexOf('2階') !== -1) {
+            text += 'include ../../images/2f.svg';
+            text += '\n    '
+            text += `style g#${project.address} > rect { fill: #faa95b !important; }`;
+        } else if (project.place.indexOf('3階') !== -1) {
+            text += 'include ../../images/3f.svg';
+            text += '\n    '
+            text += `style g#${project.address} > rect { fill: #faa95b !important; }`;
+        } else if (project.place.indexOf('4階') !== -1) {
+            text += 'include ../../images/4f.svg';
+            text += '\n    '
+            text += `style g#${project.address} > rect { fill: #faa95b !important; }`;
+        } else if (project.place === '視聴覚教室') {
+            text += 'include ../../images/2f.svg';
+            text += '\n    '
+            text += `style g#east > rect { fill: #faa95b !important; }`;
+        } else if (project.place === '中庭ステージ') {
+            text += 'include ../../images/1f.svg';
+            text += '\n    '
+            text += `style g#stage > rect { fill: #faa95b !important; }`;
+        }
         fs.writeFile(`src/projects/${project.address}.pug`, text);
     }
 }
