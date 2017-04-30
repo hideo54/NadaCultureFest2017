22d46a49ebdb1ec5beace851b7e018ae06c6644b
hideo54 <hideo54@hideo54.com>
2017-04-30 12:47:22 +0900

    Minor fix

--- projects-generator.js
+++ projects-generator.js
@@ -14,47 +14,31 @@ block main
     h2 場所
     p ${project.place}
     `;
-        const makeStyle = (id) => {
-            return `style .${id} { fill: #faa95b !important; }`;
+        const makeMap = (floor, id) => {
+            text = `include ../../images/${floor}.svg
+    style .${id} { fill: #faa95b !important; }
+    p 色付きの区域を選択するとそのエリアのページを見ることができます。`;
+            return text;
         };
         if (project.place.indexOf('研修館') !== -1) {
-            text += 'include ../../images/2f.svg';
-            text += '\n    ';
-            text += makeStyle('training-hall');
+            text += makeMap('2f', 'training-hall');
         } else if (project.place === '西ゾーン2階') {
-            text += 'include ../../images/2f.svg';
-            text += '\n    ';
-            text += makeStyle('west');
+            text += makeMap('2f', 'west');
         } else if (project.place.indexOf('2階') !== -1) {
-            text += 'include ../../images/2f.svg';
-            text += '\n    ';
-            text += makeStyle(project.address);
+            text += makeMap('2f', project.address);
         } else if (project.place.indexOf('3階') !== -1) {
-            text += 'include ../../images/3f.svg';
-            text += '\n    ';
-            text += makeStyle(project.address);
+            text += makeMap('3f', project.address);
         } else if (project.place.indexOf('4階') !== -1) {
-            text += 'include ../../images/4f.svg';
-            text += '\n    ';
-            text += makeStyle(project.address);
+            text += makeMap('4f', project.address);
         } else if (project.place === '視聴覚教室') {
-            text += 'include ../../images/2f.svg';
-            text += '\n    ';
-            text += makeStyle('east');
+            text += makeMap('2f', 'east');
         } else if (project.place === '中庭ステージ') {
-            text += 'include ../../images/1f.svg';
-            text += '\n    ';
-            text += makeStyle('stage');
+            text += makeMap('1f', 'stage');
         } else if (project.place === '高校棟1階') {
-            text += 'include ../../images/1f.svg';
-            text += '\n    ';
-            text += makeStyle('concert');
+            text += makeMap('1f', 'concert');
         } else if (['第1グラウンド', '新中庭'].indexOf(project.place) !== -1) {
-            text += 'include ../../images/1f.svg';
-            text += '\n    ';
-            text += makeStyle(project.address);
+            text += makeMap('1f', project.address);
         }
-        text += '\n    p 色付きの区域を選択するとそのエリアのページを見ることができます。'
         fs.writeFile(`src/projects/${project.address}.pug`, text);
     }
 }
